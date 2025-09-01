e1dffa3845862e78836c507b4729fa98dbdaabab
newdevatgit <arpanwar26@gmail.com>
2025-09-01 23:58:13 +0530

    chore: initial project setup with Vite, React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center my-8">Welcome to Viharro</h1>
+    </>
+
+  )
+}
+
+export default App
